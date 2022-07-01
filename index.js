cf2ba3c0471bfaacf027feeec0c074e9631c8854
NasirUddinNahid <nahidahmed17762@gmail.com>
2022-07-02 00:47:04 +0600

    done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://DB_USER:DB_PASS@cluster0.51m8jdt.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.51m8jdt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
